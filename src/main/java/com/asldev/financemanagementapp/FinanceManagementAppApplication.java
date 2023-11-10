43b15db46e2990e46fd506a4471b334cf64621d1
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-10 07:25:42 +0500

    Create project dependencies

---
+++ src/main/java/com/asldev/financemanagementapp/FinanceManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.asldev.financemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asldev/financemanagementapp/FinanceManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asldev.financemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
