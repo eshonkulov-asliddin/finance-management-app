54d649c8d353e31a152d05f79fb598df63bf55a9
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-11 22:04:04 +0500

    Add services

---
+++ src/main/java/com/asldev/financemanagementapp/services/BudgetService.java
@@ -0,0 +1,36 @@
+package com.asldev.financemanagementapp.services;
+
+import com.asldev.financemanagementapp.entities.Budget;
+import com.asldev.financemanagementapp.repositories.BudgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BudgetService {
+
+    private final BudgetRepository budgetRepository;
+
+    @Autowired
+    public BudgetService(BudgetRepository budgetRepository) {
+        this.budgetRepository = budgetRepository;
+    }
+
+    public List<Budget> getAllBudgets() {
+        return budgetRepository.findAll();
+    }
+
+    public Optional<Budget> getBudgetById(Long id) {
+        return budgetRepository.findById(id);
+    }
+
+    public Budget saveBudget(Budget budget) {
+        return budgetRepository.save(budget);
+    }
+
+    public void deleteBudget(Long id) {
+        budgetRepository.deleteById(id);
+    }
+}
